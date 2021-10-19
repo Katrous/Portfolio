bd75c159b9ab7abcb38916351c37406ee59313ea
Katrous <45927461+Katrous@users.noreply.github.com>
2021-10-19 18:48:29 +0800

    demo redesigned & project section remade

--- Demo/includes/classes/Post.php
+++ Demo/includes/classes/Post.php
@@ -8,13 +8,26 @@ class Post {
 		$this->user_obj = new User($con, $user);
 	}
 
-	public function submitPost($body, $user_to) {
+	public function submitPost($body, $user_to, $imageName) {
 		$body = strip_tags($body); //removes html tags 
 		$body = mysqli_real_escape_string($this->con, $body);
 		$check_empty = preg_replace('/\s+/', '', $body); //Deltes all spaces 
       
 		if($check_empty != "") {
 
+			$body_array = preg_split("/\s+/", $body);
+
+			foreach($body_array as $key => $value) {
+				if(strpos($value, "www.youtube.com/watch?v=") !== false) {
+					$link = preg_split("!&!", $value);
+					$value = preg_replace("!watch\?v=!", "embed/", $link[0]);
+					$value = "<br><iframe width=\'420\' height=\'315\' src=\'" . $value . "\'></iframe><br>";
+
+					$body_array[$key] = $value;
+				}
+			}
+
+			$body = implode(" ", $body_array);
 
 			//Current date and time
 			$date_added = date("Y-m-d H:i:s");
@@ -26,7 +39,7 @@ class Post {
 				$user_to = "none";
 
 			//insert post 
-			$query = mysqli_query($this->con, "INSERT INTO posts VALUES(NULL, '$body', '$added_by', '$user_to', '$date_added', 'no', 'no', '0')");
+			$query = mysqli_query($this->con, "INSERT INTO posts VALUES(NULL, '$body', '$added_by', '$user_to', '$date_added', 'no', 'no', '0', '$imageName')");
 			$returned_id = mysqli_insert_id($this->con);
 
 			//Insert notification
@@ -69,6 +82,7 @@ class Post {
 				$body = $row['body'];
 				$added_by = $row['added_by'];
 				$date_time = $row['date_added'];
+				$imagePath = $row['image'];
 
 				//Prepare user_to string so it can be included even if not posted to a user
 				if($row['user_to'] == "none") {
@@ -206,18 +220,38 @@ class Post {
 						}
 					}
 
+					if($imagePath != "") {
+						$imageDiv = "<div class='postedImage'>
+							<img src='$imagePath' alt='test'>
+						</div>";
+					} else {
+						$imageDiv = "";
+					}
+
 					$str .= "<div class='status_post' onClick='javascript:toggle$id()'>
-								<div class='post_profile_pic'>
-									<img src='./$profile_pic' width='50'>
+								<div class='post-header'>
+
+							
+									<div class='post_profile_pic'>
+										<img src='./$profile_pic' width='50'>
+									</div>
+
+									<div class='posted_by' style='color:#ACACAC;'>
+										<a href='$added_by'> $first_name $last_name </a> 
+										<span> $user_to </span>
+									</div>
+								
+
+									<div class='right-side-header'>
+										$time_message
+										$delete_button
+									</div>
 								</div>
 
-								<div class='posted_by' style='color:#ACACAC;'>
-									<a href='$added_by'> $first_name $last_name </a> $user_to &nbsp;&nbsp;&nbsp;&nbsp;$time_message
-									$delete_button
-								</div>
 								<div id='post_body'>
 									$body
 									<br>
+									$imageDiv
 									<br>
 									<br>
 								</div>
@@ -231,7 +265,7 @@ class Post {
 							<div class='post_comment' id='toggleComment$id' style='display:none'>
 								<iframe src='comment_frame.php?post_id=$id' id='comment_iframe' frameborder='0'> </iframe>
 							</div>
-							<hr>";
+								<hr>";
 
 				}
 
@@ -314,7 +348,7 @@ class Post {
 					}
 
 					if($userLoggedIn == $added_by)
-						$delete_button = "<button class='delete_button btn-danger' id='post$id'>X</button>";
+						$delete_button = "<button class='delete_button' id='post$id'>X</button>";
 					else 
 						$delete_button = "";
 
@@ -413,17 +447,25 @@ class Post {
 					}
 
 					$str .= "<div class='status_post' onClick='javascript:toggle$id()'>
-								<div class='post_profile_pic'>
-								<img src='./$profile_pic' width='50'>
-								</div>
+								<div class='post-header'>
+									<div class='post_profile_pic'>
+									<img src='./$profile_pic' width='50'>
+									</div>
 
-								<div class='posted_by' style='color:#ACACAC;'>
-									<a href='$added_by'> $first_name $last_name </a> &nbsp;&nbsp;&nbsp;&nbsp;$time_message
+									<div class='posted_by' style='color:#ACACAC;'>
+										<a href='$added_by'> $first_name $last_name </a> 
+									</div>
+
+									<div class='right-side-header'>
+									$time_message
 									$delete_button
+									</div>
 								</div>
+								
 								<div id='post_body'>
 									$body
 									<br>
+									
 									<br>
 									<br>
 								</div>
@@ -438,6 +480,10 @@ class Post {
 								<iframe src='comment_frame.php?post_id=$id' id='comment_iframe' frameborder='0'></iframe>
 							</div>
 							<hr>";
+
+
+							
+// Image in post_body not working yet, implement later
 
 				?>
 				<script>
@@ -674,6 +720,8 @@ class Post {
 
 		echo $str;
 	}
+
+
 
 
 
--- Demo/index.php
+++ Demo/index.php
@@ -1,17 +1,54 @@
-<?php
-    include_once("includes/header.php");
-    include_once("includes/classes/User.php");
-    include_once("includes/classes/Post.php");
+<?php 
+include("includes/header.php");
 
-    if(isset($_POST['post'])) {
-        $post = new Post($con, $userLoggedIn);
-        $post->submitPost($_POST['post_text'], 'none');
-        header("Location: index.php");
-    }
+if(isset($_POST['post'])){
 
+	$uploadOk = 1;
+	$imageName = $_FILES['image']['name'];
+	$errorMessage = "";
+
+	if($imageName != "") {
+		$targetDir = "assets/images/posts/";
+		$imageName = $targetDir . uniqid() . basename($imageName);
+		$imageFileType = pathinfo($imageName, PATHINFO_EXTENSION);
+
+		if($_FILES['image']['size'] > 10000000) {
+			$errorMessage = "Sorry your file is too large";
+			$uploadOk = 0;
+		}
+
+		if(strtolower($imageFileType) != "jpeg" && strtolower($imageFileType) != "png" && strtolower($imageFileType) != "jpg") {
+			$errorMessage = "Sorry, only jpeg, jpg and png files are allowed";
+			$uploadOk = 0;
+		}
+
+		if($uploadOk) {
+			
+			if(move_uploaded_file($_FILES['image']['tmp_name'], $imageName)) {
+				//image uploaded okay
+			}
+			else {
+				//image did not upload
+				$uploadOk = 0;
+			}
+		}
+
+	}
+
+	if($uploadOk) {
+		$post = new Post($con, $userLoggedIn);
+		$post->submitPost($_POST['post_text'], 'none', $imageName);
+	}
+	else {
+		echo "<div style='text-align:center;' class='alert alert-danger'>
+				$errorMessage
+			</div>";
+	}
+
+}
     // session_destroy();
 ?>
-   <div class="user_details column">
+   <!-- <div class="user_details column">
        <a href="<?php echo $userLoggedIn?>"> <img src=".<?php echo $user['profile_pic']; ?>" alt="img test"></a>
        <div class="user_details_left_right">
         <a href="<?php echo $userLoggedIn?>">
@@ -25,12 +62,16 @@
         echo "Likes" . $user['num_likes'];
         ?>
        </div>
-   </div>
+   </div> -->
 
    <div class="main_column column">
-        <form action="index.php" method="POST" class="post_form">
+        <form action="index.php" method="POST" class="post_form" enctype="multipart/form-data">
+        <a href="<?php echo $userLoggedIn?>"> <img src="./<?php echo $user['profile_pic']; ?>" alt="img test" class='post_form_pic'></a>
             <textarea name="post_text" id="post_text" placeholder="Got something to say?"></textarea>
             <input type="submit" name="post" id="post_button" value="Post">
+            <label class="btn btn-primary">
+                <i class="fa fa-image iconPosition"> Upload a Photo </i> <input type="file" style="display: none;"  name="image">
+            </label>
         </form>
 
         
@@ -38,6 +79,7 @@
         <div class="posts_area">
 
         </div>
+        
         <img id="loading" src="assets/images/icons/loading.gif" alt="">
    </div>
 
